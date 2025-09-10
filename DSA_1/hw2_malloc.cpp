8450a9d2dc4034c68e04cf83dd533cc7db36586a
JayTangu <yade0720@gmail.com>
2025-09-11 01:58:04 +0800

    Create hw2_malloc.cpp

---
+++ DSA_1/hw2_malloc.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstring>
+#include <cstdlib>
+
+using namespace std;
+int main() {
+    int n, m;
+    cout<<"輸入人數:"<<endl;
+    cin>>n;
+    cout<<"輸入姓名長度"<<endl;
+    cin>>m;
+    cin.ignore();//因為getline的\n所以只能叫他忽略
+
+    char **names = (char**)malloc(sizeof(char*)*n); //row
+    for(int i = 0 ; i < n ; i++) {
+        names[i] = (char*)malloc((m+1)*sizeof(char)); //尾是\0 -> m+1，這是"column"
+    }
+
+    cout<<"輸入學生名字:"<<endl;
+    for(int i = 0 ; i < n ; i++) {
+        string line;
+        getline(cin, line);
+        strncpy(names[i], line.c_str(), m); //c_str是要把string改char形式
+        names[i][m] = '\0';
+    }
+
+    for(int i = n -1 ; i > 0 ; i--) {
+        for(int j = 0 ; j < i ; j++) {
+            char *name_1 = *(names + j);
+            char *name_2 = *(names + j + 1);
+
+            int lenth_1 = strlen(name_1);
+            int lenth_2 = strlen(name_2);
+
+            char last_1 = *(name_1 + lenth_1 - 1);
+            char last_2 = *(name_2 + lenth_2 - 1);
+
+            if((int)last_1 > (int)last_2) {
+                char *temp = *(names + j);
+                *(names + j) = *(names + j + 1);
+                *(names + j + 1) = temp;
+            }
+        }
+    }
+
+    cout<<endl;
+    for(int i = 0 ; i < n ; i++) {
+        cout<<*(names+i)<<endl;
+        free(*(names+i));
+    }
+    
+    free(names);
+
+    return 0;
+}
